9ae6e151dd67c62e416d6106114b5e949b27c19b
Rogerio Tadeu dos Reis <rogerio.rtr46@gmail.com>
2024-01-13 22:56:55 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/rogerioreis/curso_biblico/CursoBiblicoApplication.java
@@ -0,0 +1,13 @@
+package com.rogerioreis.curso_biblico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoBiblicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoBiblicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rogerioreis/curso_biblico/CursoBiblicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogerioreis.curso_biblico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoBiblicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
